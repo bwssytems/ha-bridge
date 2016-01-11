32a5f26ddd2ab5b1586a2a2d03570a21c9fe0273
Admin <info@bwssystems.com>
2016-01-11 16:45:49 -0600

    Added new object

---
+++ src/main/java/com/bwssystems/NestBridge/NestItem.java
@@ -0,0 +1,32 @@
+package com.bwssystems.NestBridge;
+
+public class NestItem {
+	private String name;
+	private String Id;
+	private String type;
+	private String location;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return Id;
+	}
+	public void setId(String id) {
+		Id = id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+}
