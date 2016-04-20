a717fd7c68f9dd6e6f69fc4de6b5f5cbb85724a4
Admin <info@bwssystems.com>
2016-04-20 16:44:39 -0500

    Add new classes.

---
+++ src/main/java/com/bwssystems/HABridge/api/SuccessUserResponse.java
@@ -0,0 +1,13 @@
+package com.bwssystems.HABridge.api;
+
+public class SuccessUserResponse {
+	private UserCreateResponse success;
+
+	public UserCreateResponse getSuccess() {
+		return success;
+	}
+
+	public void setSuccess(UserCreateResponse success) {
+		this.success = success;
+	}
+}
---
+++ src/main/java/com/bwssystems/HABridge/api/UserCreateResponse.java
@@ -0,0 +1,13 @@
+package com.bwssystems.HABridge.api;
+
+public class UserCreateResponse {
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
