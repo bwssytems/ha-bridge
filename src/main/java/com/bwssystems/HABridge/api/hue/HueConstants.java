ff760a488cbabe75851a77c83176fb5b4d632c9e
kvandt <kvandt@users.noreply.github.com>
2017-06-07 20:35:15 +0200

    Update HueConstants.java

--- src/main/java/com/bwssystems/HABridge/api/hue/HueConstants.java
+++ src/main/java/com/bwssystems/HABridge/api/hue/HueConstants.java
@@ -2,7 +2,7 @@ package com.bwssystems.HABridge.api.hue;
 
 public class HueConstants {
 	public final static String HUB_VERSION = "1705121051";
-	public final static String API_VERSION = "1.17.0";
+	public final static String API_VERSION = "1.19.0";
 	public final static String MODEL_ID = "BSB002";
 	public final static String UUID_PREFIX = "2f402f80-da50-11e1-9b23-";
 }
